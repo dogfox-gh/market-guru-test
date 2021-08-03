d9fb2ffc0fa5db295d44863f3176ad5137d87341
Vitalii Lysak <vitaliilysak@Vitaliis-MacBook-Pro.local>
2021-08-03 20:49:33 +0300

    upd db module and add to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+  ]
 })
-export class AppModule {}
+export class AppModule { }
