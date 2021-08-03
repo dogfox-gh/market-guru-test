d4bfe8c168359f0d04b11f7d9e69317d149dfb18
Vitalii Lysak <vitaliilysak@Vitaliis-MacBook-Pro.local>
2021-08-03 22:50:27 +0300

    add validate pipe

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,7 +1,28 @@
+import { IsNotEmpty, MinLength, IsEmail, IsEnum } from 'class-validator';
+
+enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
 export class UserDto {
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly phone: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   readonly password: string;
-  readonly gender: string;
+
+  @IsNotEmpty()
+  @IsEnum(Gender, {
+    message: 'gender must be either male or female',
+  })
+  readonly gender: Gender;
 }
