a5c6fcc2ea9ea9f19ebf33ff583c22105eec3391
Vitalii Lysak <vitaliilysak@Vitaliis-MacBook-Pro.local>
2021-08-03 21:11:54 +0300

    add user service and provider

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { usersProviders } from './users.providers';
 
-@Module({})
+@Module({
+  providers: [UsersService, ...usersProviders],
+  exports: [UsersService],
+})
 export class UsersModule {}
