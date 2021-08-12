c791c63a8a319ec09b6eadf120e7174f2da3f40a
Vitalii Lysak <vitaliilysak@Vitaliis-MacBook-Pro.local>
2021-08-12 21:59:39 +0300

    add user migration

---
+++ src/core/database/config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'my_password',
+    database: 'postgres',
+    host: '0.0.0.0',
+    port: 54320,
+    dialect: 'postgres',
+  },
+};
---
+++ src/core/database/migrations/20210812181622-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      gender: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
